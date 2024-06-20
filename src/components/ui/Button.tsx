41202b8323a330b50d028342a17fad01a1f1ad92
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-20 14:20:28 +0300

    Create button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,10 @@
-const Button = () => {
+import { cn } from "@/lib/utils"
+
+const Button = ({className, children}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <div>Button</div>
+    <button className={cn("flex p-[10px_20px] items-center justify-center gap-[10px] shrink-0 rounded-[4px] bg-[var(--primary)] hover:bg-[var(--primary-hover)] text-[var(--secondary)] transition-colors", className)}>
+      {children}
+    </button>
   )
 }
 
