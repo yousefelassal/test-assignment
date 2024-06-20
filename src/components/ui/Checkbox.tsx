1e341b0fa97886851b51b16493ee046df8215173
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-20 15:43:38 +0300

    refactor: Update Checkbox component to include label prop and improve styling

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,6 +1,21 @@
-const Checkbox = () => {
+import { cn } from "@/lib/utils"
+
+interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+}
+
+const Checkbox = ({
+  className,
+  label,
+  ...props
+}: CheckboxProps) => {
   return (
-    <div>Checkbox</div>
+    <label className={cn("checkbox-container p-[8px_15px_8px_22px] flex justify-between items-center cursor-pointer", className)}>
+      <span className="text-[14px] text-[var(--secondary)] leading-[130%]">{label}</span>
+      <label className="checkbox">
+        <input id="checkbox" className="hidden" type="checkbox" {...props} />
+      </label>
+    </label>
   )
 }
 
