d63e31ef91e971daec13001155ff880657a2136c
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-20 14:27:09 +0300

    chore: Refactor Button component to accept additional props

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,13 @@
 import { cn } from "@/lib/utils"
 
-const Button = ({className, children}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+const Button = ({
+  className,
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button className={cn("flex p-[10px_20px] items-center justify-center gap-[10px] shrink-0 rounded-[4px] bg-[var(--primary)] hover:bg-[var(--primary-hover)] text-[var(--secondary)] transition-colors", className)}>
+    <button
+      className={cn("flex p-[10px_20px] items-center justify-center gap-[10px] shrink-0 rounded-[4px] bg-[var(--primary)] hover:bg-[var(--primary-hover)] text-[var(--secondary)] transition-colors", className)} {...props}>
       {children}
     </button>
   )
