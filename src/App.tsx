d5756ff99d813c7d0630c6c653e3184812b1c0a2
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-20 15:52:10 +0300

    refactor: Update App component to include padding for larger screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Main from "./components/Main"
 
 function App() {
   return (
-    <div className="bg-white min-h-72 flex flex-col items-center justify-center w-full">
+    <div className="bg-white min-h-72 p-4 lg:p-[85px_104px_383px_104px] flex flex-col items-center justify-center w-full">
       <Main />
     </div>
   )
