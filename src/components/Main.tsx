3a06b9b795ac8fd7a92916d377d4f5bd7ccd6ce6
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-20 15:44:03 +0300

    feat: Update Main component to include Checkbox and improve layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,11 @@
 import Button from "./ui/Button"
+import Checkbox from "./ui/Checkbox"
 
 const Main = () => {
   return (
-    <div>
-      <Button>Done</Button>
+    <div className="w-full max-w-2xl">
+      <Checkbox label="Page 1" className="w-full" />
+      <Button className="w-full">Done</Button>
     </div>
   )
 }
